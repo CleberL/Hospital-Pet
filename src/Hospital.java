9e4ee20aa2eed0191e2b461515aafbffdd32d2c4
Cleber Lima <cleberlima96@gmail.com>
2018-11-19 19:26:18 -0300

    adicionado internação

--- src/Hospital.java
+++ src/Hospital.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
 public class Hospital {
-	
-	private ArrayList<Animal> internados = new ArrayList<Animal>();
-	
-	public void tosar(Animal a) {
 
+	private ArrayList<Animal> internados = new ArrayList<Animal>();
+
+	public void tosar(Animal a) {
+		
 	}
 
 	public void darBanho(Animal a) {
@@ -28,14 +28,31 @@ public class Hospital {
 			a.setDoente(false);
 			System.out.println(a.getNome() + "foi medicado e está saudável");
 		}
-	}	
-
-	private void internar(Animal a) {
-		//TODO implementar internação
 	}
 
-	private void darAlta() {
-		//TODO implementar alta
+	public void internar(Animal a) {
+		int indice = internados.indexOf(a);
+		if (indice != -1) {
+			System.out.println("Este animal já está internado");
+		}else {
+			internados.add(a);
+			System.out.println("O" + a.getClass() + "foi internado");
+		}
+	}
+
+	public void darAlta(Animal a) {
+		if (internados.isEmpty()) {
+			System.out.println("Não há animais internados");
+		} else {
+			int indice = internados.indexOf(a);
+			if (indice == -1) {
+				System.out.println("Este animal não está internado");
+			}else {
+				internados.remove(indice);
+				System.out.println("O" + a.getClass() + "não está no hospital");
+			}
+		}
+
 	}
 
 }
