a51249a848e9298fa1d9dd777b525ede305cb9ba
Cleber Lima <cleberlima96@gmail.com>
2018-11-20 17:29:37 -0300

    try/catch

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,37 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Hospital upe = new Hospital();
-		
+
 		Animal a1 = new Cachorro("toto");
 		Animal a2 = new Gato("nik", 12);
 		Animal a3 = new Gato("pingo", 6);
-		
-		Veterinario v = new Veterinario("adriano");
-		
-		upe.inserirVeterinario(v);
-		
-		
-		if (a1 instanceof Cachorro) {
-			((Cachorro) a1).adestrar();
-		}
-		
-		if (a2 instanceof Gato) {
-			((Gato) a2).miar();
-		}
 
-		v.consultar(a3);		
-		
-		v.medicar(a3);
-		v.vacinar(a3);
-		
-		v.consultar(a3);
-		
-		
-		upe.pagarComissaoVeterinarios();
+		Veterinario v = new Veterinario("adriano");
+
+		try {
+			upe.inserirVeterinario(v);
+
+			if (a1 instanceof Cachorro) {
+				((Cachorro) a1).adestrar();
+			}
+
+			if (a2 instanceof Gato) {
+				((Gato) a2).miar();
+			}
+
+			v.consultar(a3);
+
+			v.medicar(a3);
+			v.vacinar(a3);
+
+			v.consultar(a3);
+
+			upe.pagarComissaoVeterinarios();
+		} catch (Exception e) {
+			System.err.println(e);
+		}
 	}
 
 }
